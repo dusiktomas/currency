dddb97b0944ec84f3c230b55452f94138a2d312f
toje <root@t1.web43.cz>
2015-04-01 16:09:39 +0200

    edit post

--- msg-builder.php
+++ msg-builder.php
@@ -3,5 +3,7 @@ header('Content-type: application/json');
 require 'classes/Database.php';
 require 'classes/MessageGraphBuilder.php';
 
-$data = new MessageGraphBuilder();
-return print(json_encode($data->buildGraphData()));
+if(isset($_POST)){
+  $data = new MessageGraphBuilder();
+  return print(json_encode($data->buildGraphData()));
+}
